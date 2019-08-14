3f8757b7bd15e7f78335cc242e04129d1a19e8aa
hypnot <dayue12168@163.com>
2019-08-14 11:33:06 +0800

    话题内容页面

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--  CSRF Token  -->
     <meta name="csrf-token" content="{{  csrf_token() }}">
-    <title>@yield('title','LaraBBS') - Laravel进阶教程</title>
+    {{--<title>@yield('title','LaraBBS') - Laravel进阶教程</title>--}}
+    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+    <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')" />
 
     <!--  Style  -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
