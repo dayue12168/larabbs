b1ac78ab43228cb9e561dffb4e1d08f47b7c91e2
hypnot <dayue12168@163.com>
2019-08-14 16:03:08 +0800

    修复 XSS 注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -21,6 +21,8 @@ class TopicObserver
 
     public function saving(Topic $topic)
     {
+        // HTMLPurifier for Laravel 是对 HTMLPurifier 针对 Laravel 框架的一个封装。本章节中，我们将使用此扩展包来对用户内容进行过滤
+        $topic->body = clean($topic->body, 'user_topic_body');
         //excerpt 字段存储的是话题的摘录，将作为文章页面的 description 元标签使用，有利于 SEO 搜索引擎优化。
         $topic->excerpt = make_excerpt($topic->body);
     }
