639851ab72be1797f6e34ebfd0ea6a8a5b850125
hypnot <dayue12168@163.com>
2019-08-15 10:50:03 +0800

    用户可以发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -11,7 +11,15 @@ class ReplyObserver
 {
     public function creating(Reply $reply)
     {
-        //
+        //XSS 安全威胁 使用 HTMLPurifier 来修复此问题
+        $reply->content = clean($reply->content, 'user_topic_body');
+    }
+
+    public function created(Reply $reply)
+    {
+        // $reply->topic->increment('reply_count', 1);
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
     }
 
     public function updating(Reply $reply)
