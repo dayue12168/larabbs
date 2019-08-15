52871a58e5b6b91a9a99483235bd2d29dea9a14e
hypnot <dayue12168@163.com>
2019-08-15 14:37:58 +0800

    删除评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -19,8 +19,7 @@ class ReplyObserver
     public function created(Reply $reply)
     {
         // $reply->topic->increment('reply_count', 1);
-        $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
+        $reply->topic->updateReplyCount();
         // 通知话题作者有新的评论
         $reply->topic->user->notify(new TopicReplied($reply));
 
@@ -30,4 +29,10 @@ class ReplyObserver
     {
         //
     }
+
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->updateReplyCount();
+    }
+
 }
